61286364b5e9dcc03000dd9d0df11afef04e870e
Doves788 <mhrdevesh@gmail.com>
2026-06-15 18:35:57 +0530

    Fixed cookie security for Vercel

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 
-
 export const googleAuth = async (req,res) => {
     try {
         const {name , email} = req.body
@@ -14,28 +13,28 @@ export const googleAuth = async (req,res) => {
         }
         let token = await genToken(user._id)
         res.cookie("token" , token , {
-            http:true,
-            secure:false,
-            sameSite:"strict",
-            maxAge:7 * 24 * 60 * 60 * 1000
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
         return res.status(200).json(user)
 
-
-
     } catch (error) {
         return res.status(500).json({message:`Google auth error ${error}`})
     }
-    
 }
 
 export const logOut = async (req,res) => {
     try {
-        await res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
         return res.status(200).json({message:"LogOut Successfully"})
     } catch (error) {
          return res.status(500).json({message:`Logout error ${error}`})
     }
-    
 }
